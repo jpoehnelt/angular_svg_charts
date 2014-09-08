b89a8f6c98058ade0fdff1a0768a0f6c5f44d429
Justin Poehnelt <justin.poehnelt@gmail.com>
2014-09-07 17:02:40 -0700

    created a build chart function among other things

--- src/directive.js
+++ src/directive.js
@@ -1,6 +1,138 @@
 angular.module('angularChartSVG', [])
-    .directive('barChart', function ($compile) {
+    .directive('barChart', function () {
         'use strict';
+        var defaults = {
+            barHeight: 20,
+            barGap: 2,
+            barAnimate: true,
+            barAnimateDuration: '0.5s',
+            label: true,
+            labelPadding: 5,
+            labelAlign: 'left'
+        };
+
+        function applyDefaults(scope) {
+            for (var key in defaults) {
+                if (key !== 'barHeight' && defaults.hasOwnProperty(key)) {
+                    if (scope[key] === undefined) {
+                        scope[key] = defaults[key];
+                    }
+                }
+            }
+        }
+
+        function barVerticalHeight(numBars, barHeight, barGap, chartHeight) {
+            // if no bar height specified
+            if (barHeight === undefined || barHeight === null && chartHeight) {
+                return parseInt(chartHeight / numBars, 0);
+            }
+            return (barHeight || defaults.barHeight) + barGap;
+        }
+
+        function buildChart(scope, elem) {
+            // svg elements need document.createElementNS()
+            var svgNS = "http://www.w3.org/2000/svg",
+                svg,
+                barGroup,
+                labelGroup,
+                bar,
+                barHeightTotal,
+                label,
+                labelXPos,
+                labelTextAnchor,
+                barMaxWidth,
+                animate;
+
+            // Empty the element
+            angular.element(elem[0]).html('');
+            // Create the svg element
+            svg = angular.element(document.createElementNS(svgNS, "svg"));
+            svg.attr('width', scope.chartWidth);
+            svg.attr('height', scope.chartHeight);
+
+            // Apply Defaults
+            console.log(scope);
+            applyDefaults(scope);
+            console.log(scope);
+
+            // Groupings for Chart Bars and Labels
+            barGroup = angular.element(document.createElementNS(svgNS, "g"));
+            barGroup.addClass('bars');
+            if (scope.label) {
+                labelGroup = angular.element(document.createElementNS(svgNS, "g"));
+                labelGroup.attr('fill', 'white');
+                labelGroup.addClass('labels');
+            }
+
+            // Get Bar Height with Gap
+            barHeightTotal = barVerticalHeight(scope.chartData.length, scope.barHeight, scope.barGap, scope.chartHeight);
+            barMaxWidth = 0;
+            for (var i = 0; i < scope.chartData.length; i++) {
+                if (scope.chartData[i].value > barMaxWidth) {
+                    barMaxWidth = scope.chartData[i].value;
+                }
+            }
+
+            // Figure Out Label Placement
+            if (scope.labelAlign === 'right') {
+                console.log('align right');
+                labelTextAnchor = 'end';
+                labelXPos = scope.chartWidth - scope.labelPadding;
+            } else if (scope.labelAlign === 'middle') {
+                console.log('align middle');
+
+                labelTextAnchor = 'middle';
+                labelXPos = scope.chartWidth / 2;
+            } else {
+                labelTextAnchor = 'start';
+                labelXPos = scope.labelPadding;
+            }
+
+            // Iterate Data and Create Bars, Labels
+            for (var i = 0; i < scope.chartData.length; i++) {
+                var from = 0,
+                    to = scope.chartData[i].value / barMaxWidth * scope.chartWidth;
+                // Bar
+                bar = angular.element(document.createElementNS(svgNS, "rect"));
+                bar.attr('x', from);
+                bar.attr('y', i * barHeightTotal);
+                bar.attr('width', to);
+                bar.attr('height', barHeightTotal - scope.barGap);
+                bar.attr('fill', '#cccccc');
+                if (scope.barAnimate) {
+                    animate = angular.element(document.createElementNS(svgNS, "animate"));
+                    animate.attr('attributeName', 'width');
+                    animate.attr('from', from);
+                    animate.attr('to', to);
+                    animate.attr('dur', scope.barAnimateDuration);
+                    bar.append(animate);
+                }
+
+
+                barGroup.append(bar);
+
+                // Labels if desired
+                if (scope.label) {
+                    label = angular.element(document.createElementNS(svgNS, "text"));
+                    label.attr('alignment-baseline', 'middle');
+                    label.attr('text-anchor', labelTextAnchor);
+                    label.attr('x', labelXPos);
+                    label.attr('y', parseInt(i * barHeightTotal + scope.barHeight / 2), 0);
+                    label.html(scope.chartData[i].key);
+                    labelGroup.append(label);
+                }
+            }
+            // Append Groupings to SVG
+            svg.append(barGroup);
+            if (labelGroup) {
+                svg.append(labelGroup);
+            }
+
+            // Append to Element
+            elem.append(svg);
+        }
+
+
         return {
             restrict: 'E',
             scope: {
@@ -14,70 +146,20 @@ angular.module('angularChartSVG', [])
                 barValue: '&',
                 label: '=?',
                 labelText: '&',
-                labelAlign: '@',
+                labelAlign: '=?',
                 labelPadding: '=?',
                 chartEvents: '=?'
 
             },
             link: function (scope, elem, attrs) {
-                // Defaults
-                scope.barAnimate = scope.barAnimate || false;
-                scope.barAnimateDuration = scope.barAnimateDuration || '0.5s';
-                scope.barHeight = scope.barHeight || 20;
-                scope.barGap = scope.barGap || 1;
-                scope.label = scope.label || true;
-                scope.labelPadding = scope.labelPadding || 5;
-                scope.labelAlign = scope.labelAlign || 'right';
-
-                function alignLabels() {
-                    // Label Float
-                    if (scope.labelAlign === 'right') {
-                        scope.labelTextAnchor = 'end';
-                        scope.labelXPos = scope.chartWidth - scope.labelPadding;
-                    } else if (scope.labelAlign === 'center') {
-                        scope.labelTextAnchor = 'middle';
-                        scope.labelXPos = scope.chartWidth / 2;
-                    } else {
-                        scope.labelTextAnchor = 'start';
-                        scope.labelXPos = scope.labelPadding;
-                    }
-                }
-
-                function getMaxValue() {
-                    scope.maxValue = 0;
-                    for (var i = 0; i < scope.chartData.length; ++i) {
-                        if (scope.chartData[i].value > scope.maxValue) {
-                            scope.maxValue = scope.chartData[i].value;
-                        }
-                    }
-                }
-
-
-                function buildChart() {
-                    console.log(elem);
-                }
-
-
-                // Init
-                console.log(scope);
-                alignLabels();
-                getMaxValue();
-
-                // Watch/Observe Attributes
-                attrs.$observe('chartWidth', function (newValue) {
-                    getMaxValue();
-                });
-                scope.$watch('labelPadding', function (newValue) {
-                    alignLabels();
-                });
+                buildChart(scope, elem);
                 scope.$watch('chartData', function (value) {
-                    getMaxValue();
+                    buildChart(scope, elem);
                 }, true);
+                scope.$on('charts.redraw', function (e) {
+                    buildChart(scope, elem);
 
-
-            },
-            templateUrl: function (element, attr) {
-                return attr.templateUrl || '../templates/barchart.html';
+                });
             }
 
         };
